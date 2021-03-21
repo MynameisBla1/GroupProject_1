f686f498ccf816644b63a62b5b9fa2b2bfeaef9b
sarah926 <sarahtoll@trentu.ca>
2021-03-21 10:36:56 -0400

    testing rectangle

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,9 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private TextField gameOver;
+    @FXML
+    Button beginGame;
+
+
+    public void beginClicked(){
+        System.out.println("button");
+        beginGame.setDisable(true);
+        gameOver.setDisable(true);
+    }
 }
