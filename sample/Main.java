35fdd197ee35f3491b1190556ce926b79dd7784a
Y Tran <ytran@trentu.ca>
2021-03-22 19:16:38 -0400

    done with welcome scene layout

--- sample/Main.java
+++ sample/Main.java
@@ -2,9 +2,11 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +16,7 @@ public class Main extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Tetris Game Clone");
+
         Scene scene = new Scene(root,400,550);
         primaryStage.setScene(scene);
         primaryStage.show();
